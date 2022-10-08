716c60311a1fb1b61b41a47442f5044eb7d00e35
Lucas Almeida Siqueira <lucasalmeida70@gmail.com>
2022-10-08 15:20:00 -0300

    Added Mui, Emotion and Rubik fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import '@fontsource/rubik/400.css';
+import '@fontsource/rubik/500.css';
+import '@fontsource/rubik/700.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
